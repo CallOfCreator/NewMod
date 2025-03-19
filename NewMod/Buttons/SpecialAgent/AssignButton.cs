8750328834023305cd0491a4e454f1988de6f178
AndriesWorks <andries46565@gmail.com>
2025-03-19 21:09:46 +0200

    updated button for SpecialAgent

--- NewMod/Buttons/SpecialAgent/AssignButton.cs
+++ NewMod/Buttons/SpecialAgent/AssignButton.cs
@@ -17,7 +17,7 @@ namespace NewMod.Buttons.SpecialAgent
         public override int MaxUses => (int)OptionGroupSingleton<SpecialAgentOptions>.Instance.AssignMaxUses;
         public override ButtonLocation Location => ButtonLocation.BottomLeft;
         public override float EffectDuration => 0f;
-        public override LoadableAsset<Sprite> Sprite => MiraAssets.Empty;
+        public override LoadableAsset<Sprite> Sprite => MiraAssets.SpecialAgentButton;
         public override bool Enabled(RoleBehaviour role)
         {
             return role is SA;
--- NewMod/NewModAsset.cs
+++ NewMod/NewModAsset.cs
@@ -12,5 +12,6 @@ public static class NewModAsset
    public static LoadableResourceAsset Arrow { get; } = new("NewMod.Resources.Arrow.png");
    public static LoadableResourceAsset ModLogo { get; } = new("NewMod.Resources.Logo.png");
    public static LoadableResourceAsset Camera { get; } = new("NewMod.Resources.cam.png");
+   public static LoadableResourceAsset SpecialAgentButton { get; } = new("NewMod.Resources.givemission.png");
    public static LoadableAudioResourceAsset ReviveSound { get; } = new("NewMod.Resources.Sounds.revive.wav");
 }
