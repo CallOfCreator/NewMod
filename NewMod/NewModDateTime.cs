e305cbf73683f13141e6efeff8f882cbc77d772a
CallOfCreator <idiotscavenger@gmail.com>
2025-09-02 20:28:04 +0000

    Prep for possible Halloween lobby

--- NewMod/NewModDateTime.cs
+++ NewMod/NewModDateTime.cs
@@ -8,12 +8,12 @@ namespace NewMod
         private const int Day = 28;
         private const int Hour = 16;
         public static readonly TimeSpan Window = TimeSpan.FromDays(8);
-
         public static DateTime BirthdayStartThisYear =>
             new(DateTime.Now.Year, Month, Day, Hour, 0, 0, DateTimeKind.Local);
 
         public static DateTime BirthdayStartNextYear =>
             new(DateTime.Now.Year + 1, Month, Day, Hour, 0, 0, DateTimeKind.Local);
+
         public static DateTime UpcomingBirthdayStart
         {
             get
@@ -28,7 +28,9 @@ namespace NewMod
                 return BirthdayStartNextYear;
             }
         }
+
         public static DateTime BirthdayWindowEndThisYear => BirthdayStartThisYear + Window;
+
         public static bool IsNewModBirthdayWeek
         {
             get
@@ -40,5 +42,17 @@ namespace NewMod
             }
         }
         public static bool IsWraithCallerUnlocked => DateTime.Now >= BirthdayStartThisYear;
+        private const int HalloweenMonth = 10;
+        private const int HalloweenDay = 31;
+        public static DateTime HalloweenThisYear =>
+            new(DateTime.Now.Year, HalloweenMonth, HalloweenDay, 0, 0, 0, DateTimeKind.Local);
+        public static bool IsNewModHalloween
+        {
+            get
+            {
+                var now = DateTime.Now;
+                return now.Date == HalloweenThisYear.Date;
+            }
+        }
     }
 }
