3e4db5bba69c120219c1e6de8a1f62372bbbc4c9
CallOfCreator <idiotscavenger@gmail.com>
2026-01-21 20:45:22 +0000

    Removed the local FPS setting

--- NewMod/LocalSettings/NewModLocalSettings.cs
+++ NewMod/LocalSettings/NewModLocalSettings.cs
@@ -17,14 +17,6 @@ namespace NewMod.LocalSettings
             TabIcon = NewModAsset.NMIcon,
         };
 
-        [LocalSliderSetting("Frame Rate Limit", min: 30f, max: 240f, "Lock your framerate (FPS)", displayValue: true, formatString: "0", roundValue: true)]
-        public ConfigEntry<float> FrameRateLimit { get; } = config.Bind(
-            "Performance",
-            "FrameRateLimit",
-            165f,
-            "Frames per second limit"
-        );
-
         [LocalToggleSetting("Enable Custom Cursor", "Enable the custom cursor from the birthday update")]
         public ConfigEntry<bool> EnableCustomCursor { get; } = config.Bind(
             "Features",
@@ -44,11 +36,6 @@ namespace NewMod.LocalSettings
         public override void OnOptionChanged(ConfigEntryBase configEntry)
         {
             base.OnOptionChanged(configEntry);
-
-            if (configEntry == FrameRateLimit)
-            {
-                Application.targetFrameRate = (int)FrameRateLimit.Value;
-            }
 
             if (configEntry == AlwaysButtonsLeft && Application.platform == RuntimePlatform.Android)
             {
