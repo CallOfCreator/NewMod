1917ce0c6fabe0611175aed8d141bf2991f48391
CallOfCreator <idiotscavenger@gmail.com>
2026-01-27 11:10:54 +0000

    Fix stats issues on Android

--- NewMod/Patches/StatsPopupPatch.cs
+++ NewMod/Patches/StatsPopupPatch.cs
@@ -14,7 +14,7 @@ namespace NewMod.Patches
 {
     public static class CustomStatsManager
     {
-        private static readonly string SavePath = Path.Combine(Application.consoleLogPath, "customStats.dat");
+        private static readonly string SavePath = Path.Combine(Application.persistentDataPath, "customStats.dat");
         public static Dictionary<string, int> CustomRoleWins = new();
         public static bool _loaded = false;
         public static void SaveCustomStats()
