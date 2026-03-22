3927546baa19196d18b963e55cdc87364c15771b
CallOfCreator <idiotscavenger@gmail.com>
2026-03-22 20:24:19 +0100

    cleaning

--- NewMod/Buttons/RevivedKillButton.cs
+++ NewMod/Buttons/RevivedKillButton.cs
@@ -4,7 +4,6 @@ using MiraAPI.Utilities.Assets;
 using NewMod.Roles.ImpostorRoles;
 using Reactor.Utilities;
 using AmongUs.GameOptions;
-using System.Linq;
 using UnityEngine;
 using MiraAPI.Networking;
 using MiraAPI.Utilities;
@@ -20,18 +19,17 @@ namespace NewMod.Buttons
         public override MiraKeybind Keybind => MiraGlobalKeybinds.PrimaryAbility;
         public override ButtonLocation Location => ButtonLocation.BottomRight;
         public override LoadableAsset<Sprite> Sprite => NewModAsset.VanillaKillButton;
+
         public override bool Enabled(RoleBehaviour role)
         {
             return NecromancerRole.RevivedPlayers.ContainsKey(PlayerControl.LocalPlayer.PlayerId);
         }
+
         public override PlayerControl GetTarget()
         {
             return PlayerControl.LocalPlayer.GetClosestPlayer(true, Distance);
         }
-        public override bool IsTargetValid(PlayerControl target)
-        {
-            return target.PlayerId != PlayerControl.LocalPlayer.PlayerId;
-        }
+
         public override void SetOutline(bool active)
         {
             Target.cosmetics.SetOutline(active, new Il2CppSystem.Nullable<Color>(Palette.ImpostorRed));
@@ -39,7 +37,6 @@ namespace NewMod.Buttons
 
         public override bool CanUse()
         {
-            if (!NecromancerRole.RevivedPlayers.ContainsKey(PlayerControl.LocalPlayer.PlayerId)) return false;
             return true;
         }
 
